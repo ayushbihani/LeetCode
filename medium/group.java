46c3b8a4e308e75b306064da99f5200f5f4c4b90
Bihani <Ayush.Bihani@shell.com>
2019-07-04 14:54:50 +0530

    Added groups solutions

---
+++ medium/group.java
@@ -0,0 +1,13 @@
+/**
+ * Group anagrams
+ */
+
+ class group{
+     public static void main(String[] args){
+         int i = 1;
+         for(int j = 20; j < 23;j++ ){
+             i^= j;
+         }
+         System.out.println(i);
+     }
+ }
